55e17575dbe78a52c63eb91a7400d22809e035fc
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-04-22 20:31:17 -0300

    Campos do usuario e exportação do schema

---
+++ backend/models/User.js
@@ -0,0 +1,35 @@
+const { required } = require('joi');
+const mongoose = require('mongoose');
+
+const usuarioSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  hashed_password: {
+    type: String,
+    required: True
+  },
+  is_Active: {
+    type: Boolean,
+    required: True
+  }
+});
+
+// Criando o model a partir do schema
+const Usuario = mongoose.model('Usuario', usuarioSchema);
+
+module.exports = Usuario;
