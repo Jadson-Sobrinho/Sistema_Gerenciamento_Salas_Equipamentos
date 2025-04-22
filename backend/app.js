a181dd091b44d70bddcf5b4cf5f679bdf173d8d5
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-04-22 20:20:47 -0300

    Conexão com o banco de dados

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Olá, mundo!');
+  res.send('API rodando!');
 });
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
+module.exports = app;
