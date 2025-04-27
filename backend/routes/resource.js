2b4693c2ffcef464809ce0bc4f35a38ecd7b29f5
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-04-27 19:06:18 -0300

    Refactor - Modularizando as chamadas das rotas com o controller

--- backend/routes/resource.js
+++ backend/routes/resource.js
@@ -1,75 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const Sala = require('../models/Room');
+const resourceController = require('../controllers/resourceController');
 
-router.get('/', async (req, res) => {
-    try {
-        console.log("Rota /sala chamada");
-        const salas = await Sala.find();
-        console.log(salas);
-        res.send(salas);
-    } catch (err) {
-        res.status(500).json({ erro: 'Erro ao buscar salas.' });
-    }
-});
+// [GET] /room- Busca todas as salas
+router.get('/', resourceController.getAllRooms);
 
 // [GET] /room/:number - Busca uma sala pelo numero
-router.get('/:room_number', async (req, res) => {
-    try {
-      console.log("rota number chamada");
-      const salas = await Sala.findOne({ room_number: req.params.room_number });
-      console.log(salas);
-      if (!salas) return res.status(404).json({ erro: 'Sala não encontrada.' });
-      res.json(salas);
-    } catch (err) {
-      res.status(500).json({ erro: 'Erro ao buscar a sala.' });
-    }
-  });
-
+router.get('/:room_number', resourceController.getRoomByNumber);
 
 // POST /room - cria nova sala
-router.post('/', async (req, res) => {
-    try {
-        console.log("Rota /room [POST]");
-      const {
-        name,
-        room_number,
-        type,
-        capacity,
-        tags,
-        module,
-        floor,
-        status,
-        available_hours,
-        description
-      } = req.body;
-      
-      console.log(req.body);
-
-      const newRoom = new Sala({
-        name,
-        room_number,
-        type,
-        capacity,
-        tags,
-        module,
-        floor,
-        status,
-        available_hours,
-        description
-      });
-      
-
-      const savedRoom = await newRoom.save();
-      res.status(201).json(savedRoom);
-  
-    } catch (error) {
-      if (error.code === 11000) {
-        res.status(400).json({ message: 'Número da sala já está em uso.' });
-      } else {
-        res.status(500).json({ message: 'Erro ao criar sala.', error });
-      }
-    }
-  });
+router.post('/', resourceController.registerRoom);
   
 module.exports = router;
