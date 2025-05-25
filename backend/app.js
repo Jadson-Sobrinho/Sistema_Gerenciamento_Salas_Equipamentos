d25f4db3cefb8c079d32b6c0f2836ef289b130dc
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-05-25 15:40:52 -0300

    Feat - Uso do cors para permitir que o front acesse os formularios

--- backend/app.js
+++ backend/app.js
@@ -9,9 +9,21 @@ const reservaRoute = require('./routes/reserve');
 
 app.use(cors());                          
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend/src/pages/index.html')));
+app.use(express.static(path.join(__dirname, '../frontend/src/pages')));
 
-app.use(express.json());
+
+app.get('/reserve-form', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/src/pages/index.html'));
+});
+
+app.get('/room-form', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/src/pages/resource.html'));
+});
+
+app.get('/user-form', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/src/pages/user.html'));
+});
+
 app.use('/user', usuariosRoute);
 app.use('/room', salasRoute);
 app.use('/reserve', reservaRoute);
