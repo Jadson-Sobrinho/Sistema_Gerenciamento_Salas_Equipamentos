77a53e975d4be7da8f51bb044a6c97127c74fdee
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-05-25 11:45:30 -0300

    Feat - Cadastro de reservas

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');              
+const path = require('path'); 
 
 const usuariosRoute = require('./routes/user');
 const salasRoute = require('./routes/resource');
 const reservaRoute = require('./routes/reserve');
+
+app.use(cors());                          
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend/src/pages/index.html')));
 
 app.use(express.json());
 app.use('/user', usuariosRoute);
