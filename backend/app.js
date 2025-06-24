ef5ce434d873c7cde07a355b926f3e5dd734a784
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-06-23 23:05:33 -0300

    Chore - Inicializando a aplicação pela página de login

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend/src/pages')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/src/pages/login.html'));
+});
 
 app.get('/reserve-form', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/src/pages/index.html'));
